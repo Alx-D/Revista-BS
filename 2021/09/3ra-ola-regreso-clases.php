4ce16e80479cb443fb49c36b687065df9f145a1b
Alx-D <duartepublicidad@gmail.com>
2021-10-05 20:37:12 -0700

    prueba header footer php

--- 2021/09/3ra-ola-regreso-clases.php
+++ 2021/09/3ra-ola-regreso-clases.php
@@ -25,94 +25,12 @@
 </head>
 
 <body>
-  <header class="contenedor">
-
-    <!-- HEADER MOBILE -->
-    <div class="header-mobile-cont">
-
-      <div class="header-mobile-row-1">
-        <i class="fas fa-bars" onclick="showMenu()"></i>
-
-        <div class="logo-mobile-bx">
-          <a href="/index.html">
-            <img src="/imgs/Revista-BS-logo-m.png">
-          </a>
-        </div>
-
-        <i class="fas fa-search"></i>
-      </div>
-
-      <div class="lema-mobile-bx">
-        <img src="/imgs/lema.png">
-      </div>
-
-      <nav>
-        <div class="menu-mobile" id="menu-enlaces">
-          <i class="fas fa-times" onclick="hideMenu()"></i>
-          <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Revista</a></li>
-            <li><a href="#">Artículos por tema</a></li>
-            <li><a href="#">Artículos por especialidad médica</a></li>
-            <li><a href="#">Directorio médico</a></li>
-            <li><a href="#">Femeson</a></li>
-            <li><a href="#">Contacto</a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-
-    <!-- HEADER DESKTOP -->
-    <div class="header-cont">
-
-      <div class="header-col-izq">
-        <div class="revista-bs-logo-bx">
-          <a href="/index.html">
-            <img class="logo-header" src="/imgs/Revista-BS-logo-header.png" alt="Revista BS">
-          </a>
-        </div>
-      </div>
-
-      <div class="header-col-der">
-        <div class="header-row-1">
-          <div class="facebook">
-            <a class="f1" href="https://www.facebook.com">
-              <i class="fab fa-facebook fa-lg"></i> Revista BS
-            </a>
-          </div>
-          <p class="f2 femeson-leyenda">Órgano de vinculación y divulgación de <br> la Federación Médica de Sonora</p>
-          <div class="femeson-logo-bx">
-            <img src="/imgs/femeson-escudo.png" alt="">
-          </div>
-        </div>
-
-        <div class="header-row-2">
-          <div class="lema-bx">
-            <img src="/imgs/lema.png">
-          </div>
-          <div id="buscador">
-            <form>
-              <input type="text" placeholder="Buscar">
-            </form>
-          </div>
-        </div>
-
-        <nav>
-          <div class="menu-desktop">
-            <ul>
-              <li><a href="#">Inicio</a></li>
-              <li><a href="#">Revista</a></li>
-              <li><a href="#">Artículos por tema</a></li>
-              <li><a href="#">Directorio médico</a></li>
-              <li><a href="#">Contacto</a></li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-
-      <div class="clear"></div>
-    </div>
+  <header>
+  <?php
+  require_once('header.php');
+  ?>
   </header>
+
 
   <!-- BANNER HORIZONTAL -->
   <div class="contenedor bnr-hz-01">
@@ -324,59 +242,10 @@
   <div class="altura-ajuste-footer"></div>
 
   <!-- FOOTER -->
-  <footer>
-    <div class="footer-row-1">
-      <div class="f1 footer-mision">COMUNICAMOS VALOR</div>
-    </div>
-
-    <div class="footer-row-2">
-      <div class="contenedor footer-cont">
-
-        <div class="footer-col-1">
-          <div class="footer-BSlogo-bx"><img src="/imgs/Revista-BS-logo-footer.png"></div>
-
-          <div class="btn-siganos-pr">
-            <a href="#">
-              <p><strong>Síganos en</strong></p>
-              <p><i class="fab fa-facebook"></i> Revista BS</p>
-            </a>
-          </div>
-        </div>
-
-        <div class="f1 footer-col-2">
-          <p class="anunciese"><strong>Anúnciese con nosotros</strong></p>
-          <p class="footer-tel">
-            <i class="fas fa-phone-alt footer-icon">&nbsp;</i>(662) 236 25 00
-          </p>
-          <a class="footer-email" href="mailto:publicidad@buenasalud.org">
-            <i class="fas fa-envelope footer-icon">&nbsp;</i>publicidad@buenasalud.org </a>
-          <p class="footer-direccion"><i class="fas fa-map-marker-alt footer-icon">&nbsp;</i>Quinta Mayor 181 Col. Las
-            Quintas, Hermosillo, Sonora, México CP 83240</p>
-        </div>
-
-        <div class="f1 footer-col-3">
-          <p><strong>©2021 Revista BS <br> Todos los derechos reservados</strong></p>
-          <p>Sin previa autorización, queda expresamente prohibida la publicación, retransmisión, edición y
-            cualquier otro uso de los contenidos.</p>
-          <a href="#">Política de Privacidad</a>
-          <a style="display: inline-block;" href="#">Términos y Condiciones</a>
-        </div>
-
-        <div class="btn-siganos">
-          <a href="#">
-            <p><strong>Síganos en</strong></p>
-            <p><i class="fab fa-facebook"></i> Revista BS</p>
-          </a>
-        </div>
-
-      </div>
-
-      <div class="duarte-firma">
-        <a href="mailto:duartepublicidad@gmail.com">Sitio desarrollado por <strong>Duarte Publicidad</strong></a>
-      </div>
-
-    </div>
-    <div class="wid"></div>
-  </footer>
+<footer>
+<?php
+require_once('footer.php');
+?>
+</footer>
 </body>
 </html>
