8690a61be90db7d25aaf48f120ceda3c0b095118
Alx-D <duartepublicidad@gmail.com>
2022-04-29 10:22:13 -0700

    29 abr 2022 10:20 am

--- index.php
+++ index.php
@@ -1,5 +1,117 @@
+<!DOCTYPE html>
+<html lang="es">
 
-<?php include 'header.php' ?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="icon" type="image/png" href="/imgs/Revista-BS-favicon.png">
+  <link rel="apple-touch-icon" href="/imgs/apple-touch-icon.png">
+  <meta name="description" content="Artículos sobre el cuidado de la salud y prevención de enfermedades">
+  <meta name="keywords"
+    content="salud, saludable, vida, cuidado, bienestar, prevención, enfermedades, nutrición, psicología, niños, familia, adultos mayores, sonora">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;1,300;1,400&display=swap"
+    rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Literata:ital,wght@0,300;0,400;1,400&display=swap"
+    rel="stylesheet">
+  <title>Revista BS | Salud para vivir con sentido</title>
+</head>
+
+<body>
+  <header class="contenedor">
+
+    <!-- HEADER MOBILE -->
+    <div class="header-mobile-cont">
+
+      <div class="header-mobile-row-1">
+        <i class="fas fa-bars" onclick="showMenu()"></i>
+
+        <div class="logo-mobile-bx">
+          <a href="/index.html">
+            <img src="imgs/Revista-BS-logo-m.png">
+          </a>
+        </div>
+
+        <i class="fas fa-search"></i>
+      </div>
+
+      <div class="lema-mobile-bx">
+        <img src="imgs/lema.png">
+      </div>
+
+      <nav>
+        <div class="menu-mobile" id="menu-enlaces">
+          <i class="fas fa-times" onclick="hideMenu()"></i>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Ediciones</a></li>
+            <li><a href="alianzas.html">Doctores colaboradores</a></li>
+            <li><a href="#">Entrevistas</a></li>
+            <li><a href="#">Eventos</a></li>
+            <li><a href="#">Artículos por especialidad médica</a></li>
+            <li><a href="enlaces.html">Contacto</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <!-- HEADER DESKTOP -->
+    <div class="header-cont">
+
+      <div class="header-col-izq" style="position: relative;">
+        <div class="revista-bs-logo-bx">
+          <a href="#">
+            <img src="imgs/Revista-BS-logo-header.png" alt="Revista BS">
+          </a>
+        </div>
+      </div>
+
+      <div class="header-col-der">
+        <div class="header-row-1">
+          <div class="facebook">
+            <a class="f1" href="https://www.facebook.com/RevistaBS" target="_blank">
+              <i class="fab fa-facebook fa-lg"></i> Revista BS
+            </a>
+          </div>
+          <p class="f2 femeson-leyenda">Órgano de vinculación y divulgación de <br> la Federación Médica de Sonora</p>
+          <div class="femeson-logo-bx">
+            <img src="imgs/femeson-escudo.png" alt="">
+          </div>
+        </div>
+
+        <div class="header-row-2">
+          <div class="lema-bx">
+            <img src="imgs/lema.png">
+          </div>
+          <div id="buscador">
+            <form>
+              <input type="text" placeholder="Buscar">
+            </form>
+          </div>
+        </div>
+
+        <nav>
+          <div class="menu-desktop">
+            <ul>
+              <li><a class="hv-txt-01" href="#">Inicio</a></li>
+              <li><a class="hv-txt-01" href="#" target="blank">Ediciones</a></li>
+              <li><a class="hv-txt-01" href="alianzas.html">Doctores colaboradores</a></li>
+              <li><a class="hv-txt-01" href="#">Artículos por especialidad médica</a></li>
+              <li><a class="hv-txt-01" href="enlaces.html">Contacto</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <div class="clear"></div>
+    </div>
+  </header>
 
   <div class="popup">
     <button id="close">&times;</button>
@@ -35,7 +147,7 @@
     <div class="cont-mes1">
       <div class="col1-mes1">
         <div class="impresa-th-bx hv-trns-01">
-          <a href="ediciones/2022/01/Revista-BS-260.pdf" target="blank">
+          <a href="ediciones/2022/02/Revista-BS-261.pdf" target="blank">
             <p class="f2 ind-impr">EDICIÓN DIGITAL</p>
             <figure><img src="imgs/portadas/portada-Revista-BS-261.jpg" alt="Revista BS #256"></figure>
           </a>
@@ -66,7 +178,7 @@
 
         <div class="f1 mes1-top-ceja morado">ESTE MES</div>
 
-        <a href="/ediciones/2022/02/261-tipos-ortodoncia.html">
+        <a href="ediciones/2022/02/261-tipos-ortodoncia.html">
           <div class="mes1-top-foto-bx">
             <img src="ediciones/2022/02/imgs/261-Dra-Cinthia-Borbon-TIPOS-ORTODONCIA-1.jpg">
           </div>
@@ -101,7 +213,7 @@
 
   <!-- BANNER HORIZONTAL 1MES -->
   <div class="contenedor bnr-hz-01">
-    <a href="https://hondapremium.com/" target="blank"><img src="banners/2022/Honda-diciembre-2020.png"></a>
+    <a href="https://hondapremium.com/" target="blank"><img src="ediciones/2022/banners/Honda-diciembre-2020.png"></a>
   </div>
   <div class="bnr-hz-mob">Banner mobile</div>
 
@@ -145,12 +257,12 @@
         <div class="card-art1">
           <div class="f2 ceja-med" style="background: #2a6386">Salud bucal</div>
           <div class="foto-art1-bx">
-            <a href="ediciones/2022/02/261-anestesia-digital.php">
+            <a href="ediciones/2022/02/261-anestesia-digital.html">
               <img src="ediciones/2022/02/imgs/261-Dra-Yadira-Fimbres-ANESTESIA-DIGITAL-1.jpg" alt="">
             </a>
           </div>
           <div class="cbz-art1-bx">
-            <a href="ediciones/2022/02/261-anestesia-digital.php">
+            <a href="ediciones/2022/02/261-anestesia-digital.html">
               <p class="f2 blz-card-2col hv-txt-01" style="color:#40a091;">Anestesia digital</p>
               <h2 class="f1 cbz-card-2col hv-txt-01">Un poco de magia</h2>
               <p class="f1 autor-card"><span>Odont. Yadira Fimbres Fimbres</span><br>Odontopediatría</p>
@@ -358,6 +470,10 @@
 
   <div class="clear"></div>
 
+  <div class="contenedor bnr-hz-01 bnr-esp1">
+    <a href="https://www.hospitalsanjosehermosillo.com/" target="blank"><img src="imgs/Hospital-San-Jose.jpg"></a>
+  </div>
+
   <!-- 1VIDEOS -->
   <section id="vid1">
 
@@ -410,7 +526,7 @@
 
   <!-- BANNER HORIZONTAL VIDEOS -->
   <div class="contenedor bnr-hz-01">
-    <a href="https://hondapremium.com/" target="blank"><img src="banners/2022/Honda-diciembre-2020.png"></a>
+    <a href="https://hondapremium.com/" target="blank"><img src="ediciones/2022/banners/Honda-diciembre-2020.png"></a>
   </div>
 
   <!-- 1SPV SALUD PARA VIVIR -->
@@ -590,7 +706,7 @@
 
   <!-- BANNER HORIZONTAL 1CARRUSEL -->
   <div class="contenedor bnr-hz-01">
-    <a href="https://www.hospitalsanjosehermosillo.com/" target="blank"><img src="banners/2022/Hospital-San-Jose-2020.jpg"></a>
+    <a href="https://www.hospitalsanjosehermosillo.com/" target="blank"><img src="ediciones/2022/banners/Hospital-San-Jose-2020.jpg"></a>
   </div>
 
   <!-- 1EVENTOS -->
@@ -772,7 +888,58 @@
   </section>
 
   <!-- FOOTER -->
-  <?php include '\Revista_BS_website\footer.php' ?>
+  <footer>
+    <div class="footer-row-1">
+      <div class="f1 footer-mision">COMUNICAMOS VALOR</div>
+    </div>
+
+    <div class="footer-row-2">
+      <div class="contenedor footer-cont">
+
+        <div class="footer-col-1">
+          <a class="footer-BSlogo-bx" href="index.php"><img src="imgs/Revista-BS-logo-footer.png"></a>
+
+          <div class="btn-siganos-pr">
+            <a href="https://www.facebook.com/RevistaBS" target="_blank">
+              <p><strong>Síganos en</strong></p>
+              <p><i class="fab fa-facebook"></i> Revista BS</p>
+            </a>
+          </div>
+        </div>
+
+        <div class="f1 footer-col-2">
+          <p class="anunciese"><strong>Anúnciese con nosotros</strong></p>
+          <a class="footer-tel" href="tel:6622362500"><i class="fas fa-phone-alt footer-icon">&nbsp;</i> (662) 236 2500</a>
+          <a class="footer-email" href="mailto:publicidad@buenasalud.org">
+            <i class="fas fa-envelope footer-icon">&nbsp;</i>publicidad@buenasalud.org      </a>
+          <p class="footer-direccion" ><i class="fas fa-map-marker-alt footer-icon">&nbsp;</i>Quinta Mayor 181 Col. Las Quintas, Hermosillo, Sonora, México CP 83240</p>
+          </div>
+
+        <div class="f1 footer-col-3">
+          <p><strong>©2021 Revista BS <br> Todos los derechos reservados</strong></p>
+          <p>Sin previa autorización, queda expresamente prohibida la publicación, retransmisión, edición y
+            cualquier otro uso de los contenidos.</p>
+          <a href="#">Política de Privacidad</a>
+          <a style="display: inline-block;" href="#">Términos y Condiciones</a>
+        </div>
+
+        <div class="btn-siganos">
+          <a href="https://www.facebook.com/RevistaBS" target="_blank">
+            <p><strong>Síganos en</strong></p>
+            <p><i class="fab fa-facebook"></i> Revista BS</p>
+          </a>
+        </div>
+
+      </div>
+
+      <div class="duarte-firma">
+        <a href="mailto:duartepublicidad@gmail.com">Sitio desarrollado por <strong>Duarte Publicidad</strong></a>
+      </div>
+
+    </div>
+
+    <div class="wid"></div>
+  </footer>
 
   <script>
     var menu_enlaces = document.getElementById("menu-enlaces");
