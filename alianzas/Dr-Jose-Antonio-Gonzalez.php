88f172b9f783cc642285ffdea8f60095de93559d
Alx-D <duartepublicidad@gmail.com>
2022-06-01 11:56:38 -0700

    01 Jun 2022 12 pm

---
+++ alianzas/Dr-Jose-Antonio-Gonzalez.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/alianzas.css">
+  <link rel="stylesheet" href="/css/all.min.css">
+  <meta name="description" content="Artículos, entrevistas del Doctor Joel Verdugo Correo en Revista BS">
+  <meta name="keywords"
+    content="Dr Doctor Joel Verdugo Correa nefrólogo, nefrología, enfermedad renal, enfermedades renales, salud, prevención, hermosillo, sonora">
+  <link rel="icon" type="image/png" href="/imgs/Revista-BS-favicon.png">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+  <title>Dr. José Antonio González Rincón | Revista BS</title>
+</head>
+
+<body>
+  <main class="contenedor">
+    <div class="pleca-alnz">
+      <div class="desktop">
+        <a class="f1 inicio hv-bck" href="/alianzas.html"><b>INICIO</b></a>
+        <a class="logo" href="/index.php">
+          <img src="/imgs/Revista-BS-logo-250x195.png" alt="">
+        </a>
+      </div>
+
+      <div class="f1 mobile">
+        <div class="foto w15">
+          <img src="/alianzas/imgs/Dr-Jose-Antonio-Gonzalez-th.jpg" alt="">
+        </div>
+        <div>
+          <h3>Dr. José Antonio González Rincón</h3>
+          <p>Especialidad médica</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="nombre-bx">
+      <h6 class="f3">Salud para vivir con sentido</h6>
+      <h1 class="f1 nombre">Dr. José Antonio González Rincón</h1>
+      <div class="f1 espl-ind">Especialidad médica</div>
+      <div class="f1 especialidad">Traumatología <br> y Ortopedia</div>
+      <p class="f3 cedulas">Cédula profesional: 5084256, Universidad Michoacana de San Nicolás de Hidalgo</p>
+      <p class="f3 cedulas">Cédula de especialidad: 7342886, Universidad Nacional Autónoma de México</p>
+    </div>
+
+    <div class="foto-col">
+      <div class="foto-bx">
+        <img src="/alianzas/imgs/Dr-Jose-Antonio-Gonzalez.jpg" alt="">
+        <div class="f1 agende">
+          <a href="tel:+526622161492">Llame para agendar una cita 662 216-1492</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="f1 contacto-bx">
+      <h3 class="secc-cbz">CONSULTA EN</h3>
+      <p class="ctc-empresa">Centro Médico Navarrete</p>
+      <p class="ctc-info"><i class="fas fa-map-marker-alt"></i> Blvd. Navarrete #97, esquina con Américas, <br>
+        Hermosillo, Sonora</p>
+
+      <button type="button" class="f1 colapsable">Abrir ubicación en mapa <i class="fa fa-chevron-down"></i></button>
+
+      <div class="mapa">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d217.91720904592512!2d-110.97135128587185!3d29.08573711808838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ce843fac21686d%3A0x921d66eec2f39e58!2sCentro%20M%C3%A9dico%20Navarrete!5e0!3m2!1ses-419!2smx!4v1654108061521!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <a class="inbl" href="tel:+526622161492">
+        <p class="ctc-info"><i class="fas fa-phone-alt"></i> (662) 216-1492</p>
+      </a>
+      <a class="inbl" href="tel:+52662229-0213" title="Urgencias">
+        <p class="ctc-info"><i class="fas fa-exclamation-circle" style="color: red;"></i> (662) 229-0213</p>
+      </a>
+
+      <p class="ctc-info msite"><a href="https://www.columnahermosillo.com/" class="hv-txt"
+          target="blank">www.columnahermosillo.com</a> <i class="fas fa-mouse-pointer"></i>
+      </p>
+
+      <div class="f1 redes-bx">
+        <a class="hv-trns-5" href="https://www.facebook.com/Columna-hermosillo-1534276236872116" target="blank"><img
+            src="/alianzas/imgs/facebook-logo-120x120.png" alt=""
+            title="@Columna-hermosillo, click para abrir en Facebook"></a>
+
+        <a class="hv-trns-5" href="https://www.facebook.com/OrtopediaSonora" target="blank"><img
+            src="/alianzas/imgs/facebook-logo-120x120.png" alt="" target="blank"
+            title="@OrtopediaSonora, click para abrir en Facebook"></a>
+
+        <a class="hv-trns-5" href="https://www.instagram.com/joseantoniogonzalezrincon/" target="blank"><img
+            src="/alianzas/imgs/instagram-logo-120x120.png" alt=""
+            title="@joseantoniogonzalezrincon, click para abrir en Instagram"></a>
+
+        <a class="hv-trns-5" href="https://wa.me/526628478861" target="blank"><img
+          src="/alianzas/imgs/whatsapp-logo-120x120.png" alt=""
+          title="@joseantoniogonzalezrincon, click para abrir en Instagram"></a>
+
+        <p class="f3 click-redes">Click para acceder a sus redes sociales</p>
+      </div>
+
+      <div class="sep"></div>
+    </div>
+
+    <div class="f1 formacion-bx">
+      <h3 class="secc-cbz">FORMACIÓN</h3>
+      <p class="secc-txt"><b></b> Originario de La Piedad, Michoacán, el doctor José Antonio González Rincón es médico
+        cirujano por la Universidad
+        Michoacana de San Nicolás de Hidalgo, es especialista en traumatología y ortopedia por la Universidad Nacional
+        Autónoma de México, realizó un curso en adiestramiento en cirugía de columna vertebral y deformidades y está
+        certificado por el Consejo Mexicano de Ortopedia y Traumatología, A. C.</p>
+
+      <div class="sep"></div>
+    </div>
+
+    <div class="f1 experiencia-bx">
+      <h3 class="secc-cbz">EXPERIENCIA</h3>
+      <ul class="secc-txt" style="color: magenta;">
+        <li>Lorem ipsum dolor sit amet consectetur</li>
+        <li>Beatae dolorem, commodi quod laudantium assumenda</li>
+        <li>vitae magni corporis, sint minus architecto</li>
+      </ul>
+
+      <div class="sep"></div>
+    </div>
+
+    <section class="colab-bx">
+      <div class="cbz-bx">
+        <h2 class="f1 secc-cbz">COLABORACIONES EN</h2>
+        <div>
+          <img src="/imgs/Revista-BS-logo-m.png" alt="">
+        </div>
+      </div>
+
+      <div class="cards-bx">
+
+REHACER CLASES DE CARDS
+
+        <a class="card hz hv-card" href="/ediciones/2022/01/260-ortopedia-infancia.php">
+
+          <div class="w50"><img src="/ediciones/2022/01/imgs/260-Dr-Jose-Antonio-Gonzalez-ORTOPEDIA-NINOS-1.jpg"
+              alt=""></div>
+
+          <div class="cbz-card-hz-bx">
+            <div class="f3 card-hz-ind">ARTÍCULO</div>
+            <p class="f2 card-hz-blz">Problemas ortopédicos en la infancia:</p>
+            <h4 class="f1 card-hz-cbz hv-txt">Árbol que crece derecho</h4>
+          </div>
+        </a>
+
+        <a class="card-vl hv-card" href="/2022/01/260-ortopedia-infancia.html">
+          <div class="f3 card-vl-ind">ARTÍCULO</div>
+          <img src="/2022/01/imgs/260-Dr-Jose-Antonio-Gonzalez-ORTOPEDIA-NINOS-1.jpg" alt="">
+          <div class="card-vl-cbz-bx">
+            <p class="f2">Problemas ortopédicos en la infancia:</p>
+            <h4 class="f1 hv-txt">Árbol que crece derecho</h4>
+          </div>
+        </a>
+
+        <a class="card-vl hv-card" href="/2022/01/260-ortopedia-infancia.html">
+          <div class="f3 card-vl-ind">ARTÍCULO</div>
+          <img src="/2022/01/imgs/260-Dr-Jose-Antonio-Gonzalez-ORTOPEDIA-NINOS-1.jpg" alt="">
+          <div class="card-vl-cbz-bx">
+            <p class="f2">Problemas ortopédicos en la infancia:</p>
+            <h4 class="f1 hv-txt">Árbol que crece derecho</h4>
+          </div>
+        </a>
+
+        <a class="card-vl hv-card" href="/2022/01/260-Dr-Jose-Antonio-Gonzalez.html" style="align-self:flex-start;">
+          <div class="f3 card-vl-ind" style="background: var(--verde-logo-1);">ENTREVISTA</div>
+          <div class="card-vl-cbz-bx">
+            <h4 class="f1 hv-txt">Cuidar la salud, preservar la
+              movilidad</h4>
+          </div>
+        </a>
+
+        <a class="card-vl hv-card" href="/2022/01/260-ortopedia-infancia.html">
+          <div class="f3 card-vl-ind">ARTÍCULO</div>
+          <div class="card-vl-cbz-bx">
+            <p class="f2">Problemas ortopédicos en la infancia:</p>
+            <h4 class="f1 hv-txt">Árbol que crece derecho</h4>
+          </div>
+        </a>
+
+        <iframe class="video-bx"
+          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F3069315943384795%2F&show_text=false&width=560&t=0"
+          width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+          allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          allowFullScreen="true"></iframe>
+
+        <a class="card-vl hv-card" href="/2022/01/260-ortopedia-infancia.html">
+          <div class="f3 card-vl-ind">ARTÍCULO</div>
+          <div class="card-vl-cbz-bx">
+            <p class="f2">Problemas ortopédicos en la infancia:</p>
+            <h4 class="f1 hv-txt">Árbol que crece derecho</h4>
+          </div>
+        </a>
+
+        <a class="card-vl hv-card" href="/2022/01/260-Dr-Jose-Antonio-Gonzalez.html" style="align-self:flex-start;">
+          <div class="f3 card-vl-ind" style="background: var(--verde-logo-1);">ENTREVISTaaA</div>
+          <div class="card-vl-cbz-bx">
+            <h4 class="f1 hv-txt">Cuidar la salud, preservar la
+              movilidad</h4>
+          </div>
+        </a>
+      </div>
+    </section>
+  </main>
+
+  <script>
+    var coll = document.getElementsByClassName("colapsable");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+  </script>
+</body>
+
+</html>
--- ediciones/2022/02/261-Seccion-Especial.php
+++ ediciones/2022/02/261-Seccion-Especial.php
@@ -2,91 +2,99 @@
 
 <body class="contenedor m-0a">
 
-  <header class="portada-secc-esp">
-
-    <div class="cbz-bx">
+  <header>
+    <div class="cbz-prtd-esp-bx">
       <div class="f2 logo">
         <a href="/index.php"><img src="/imgs/Revista-BS-logo-m.png"></a> &nbsp; SECCIÓN ESPECIAL
       </div>
       <h1 class="f2">Salud bucal integral</h1>
       <h3 class="f1">Dientes sanos, sonrisa deslumbrante</h2>
     </div>
+  </header>
 
-    <div class="f1 click">
-      De click sobre el doctor de su preferencia
-    </div>
-
+  <main>
     <div class="foto">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1100 960">
+      <div class="f1 click">
+        De click sobre el doctor de su preferencia
+      </div>
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1100 800">
+        <image width="1100" height="800" xlink:href="/ediciones/2022/02/imgs/261-Seccion-Especial-00.jpg"></image>
 
-        <image width="1100" height="960" xlink:href="/ediciones/2022/02/imgs/261-Seccion-Especial-00.jpg"></image>
-
-        <a xlink:href="/ediciones/2022/02/261-Dr-Alberto-Vazquez.php" xlink:title="Dr. Alberto Vázquez Zavala, Rehabilitación oral e implantología">
-          <rect x="271" y="379" fill="#fff" opacity="0" width="90" height="260"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dr-Carlos-Sanabia.php" target="blank" xlink:title="Dr. Carlos Sanabia Hurtado, Ortodoncia">
+          <rect x="188" y="82" fill="#fff" opacity="0" width="96" height="223"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Dulce-Montijo.php" xlink:title="Dra. Dulce Montijo Flores, Odontología general">
-          <rect x="422" y="427" fill="#fff" opacity="0" width="100" height="233"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Yadira-Fimbres.php" target="blank" xlink:title="Dra. Yadira Fimbres Fimbres, Odontopediatría">
+          <rect x="336" y="108" fill="#fff" opacity="0" width="101" height="114"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Karla-Lopez.php" xlink:title="Dra. Karla López Almazán, Odontopediatría">
-          <rect x="584" y="415" fill="#fff" opacity="0" width="92" height="246"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Yadira-Fimbres.php" target="blank" xlink:title="Dra. Yadira Fimbres Fimbres, Odontopediatría">
+          <rect x="355" y="224" fill="#fff" opacity="0" width="75" height="96"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Cinthia-Borbon.php" xlink:title="Dra. Cinthia Borbón Esquer, Ortodoncia">
-          <rect x="761" y="348" fill="#fff" opacity="0" width="94" height="268"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dr-German-Rios.php" target="blank" xlink:title="Dr. Germán Ríos Barceló, Ortodoncia y ortopedia maxilofacial">
+          <rect x="496" y="73" fill="#fff" opacity="0" width="100" height="199"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Cinthia-Borbon.php" xlink:title="Dra. Cinthia Borbón Esquer, Ortodoncia">
-          <rect x="856" y="455" fill="#fff" opacity="0" width="50" height="162"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dr-Hector-Delgado.php" target="blank" xlink:title="Dr. Héctor Delgado Contreras, Implantología">
+          <rect x="676" y="97" fill="#fff" opacity="0" width="100" height="194"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dr-Carlos-Sanabia.php" xlink:title="Dr. Carlos Sanabia Hurtado, Ortodoncia">
-          <rect x="174" y="236" fill="#fff" opacity="0" width="95" height="245"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Dayana-Duron.php" target="blank" xlink:title="Dra. Dayana Durón Rivas, Ortodoncia y ortopedia maxilofacial">
+          <rect x="863" y="127" fill="#fff" opacity="0" width="87" height="137"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dr-Carlos-Sanabia.php" xlink:title="Dr. Carlos Sanabia Hurtado, Ortodoncia">
-          <rect x="269" y="234" fill="#fff" opacity="0" width="50" height="133"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Dayana-Duron.php" target="blank" xlink:title="Dra. Dayana Durón Rivas, Ortodoncia y ortopedia maxilofacial">
+          <rect x="908" y="268" fill="#fff" opacity="0" width="60" height="107"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Yadira-Fimbres.php" xlink:title="Dra. Yadira Fimbres Fimbres, Odontopediatría">
-          <rect x="329" y="272" fill="#fff" opacity="0" width="110" height="104"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dr-Alberto-Vazquez.php" target="blank" xlink:title="Dr. Alberto Vázquez Zavala, Rehabilitación oral e implantología">
+          <rect x="286" y="228" fill="#fff" opacity="0" width="65" height="91"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Yadira-Fimbres.php" xlink:title="Dra. Yadira Fimbres Fimbres, Odontopediatría">
-          <rect x="364" y="366" fill="#fff" opacity="0" width="78" height="50"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dr-Alberto-Vazquez.php" target="blank" xlink:title="Dr. Alberto Vázquez Zavala, Rehabilitación oral e implantología">
+          <rect x="225" y="322" fill="#fff" opacity="0" width="151" height="196"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dr-German-Rios.php" xlink:title="Dr. Germán Ríos Barceló, Ortodoncia y ortopedia maxilofacial">
-          <rect x="491" y="226" fill="#fff" opacity="0" width="119" height="187"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Dulce-Montijo.php" target="blank" xlink:title="Dra. Dulce Montijo Flores, Odontología general">
+          <rect x="432" y="276" fill="#fff" opacity="0" width="83" height="92"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dr-Hector-Delgado.php" xlink:title="Dr. Héctor Delgado Contreras, Implantología">
-          <rect x="678" y="252" fill="#fff" opacity="0" width="81" height="254"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Dulce-Montijo.php" target="blank" xlink:title="Dra. Dulce Montijo Flores, Odontología general">
+          <rect x="405" y="369" fill="#fff" opacity="0" width="133" height="164"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Dayana-Duron.php" xlink:title="Dra. Dayana Durón Rivas, Ortodoncia y ortopedia maxilofacial">
-          <rect x="858" y="272" fill="#fff" opacity="0" width="107" height="180"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Karla-Lopez.php" target="blank" xlink:title="Dra. Karla López Almazán, Odontopediatría">
+          <rect x="601" y="253" fill="#fff" opacity="0" width="72" height="101"></rect>
         </a>
 
-        <a xlink:href="/ediciones/2022/02/261-Dra-Dayana-Duron.php" xlink:title="Dra. Dayana Durón Rivas, Ortodoncia y ortopedia maxilofacial">
-          <rect x="908" y="454" fill="#fff" opacity="0" width="67" height="71"></rect>
+        <a xlink:href="/ediciones/2022/02/261-Dra-Karla-Lopez.php" target="blank" xlink:title="Dra. Karla López Almazán, Odontopediatría">
+          <rect x="558" y="357" fill="#fff" opacity="0" width="167" height="186"></rect>
+        </a>
+
+        <a xlink:href="/ediciones/2022/02/261-Dra-Cinthia-Borbon.php" target="blank" xlink:title="Dra. Cinthia Borbón Esquer, Ortodoncia">
+          <rect x="778" y="194" fill="#fff" opacity="0" width="84" height="99"></rect>
+        </a>
+
+        <a xlink:href="/ediciones/2022/02/261-Dra-Cinthia-Borbon.php" target="blank" xlink:title="Dra. Cinthia Borbón Esquer, Ortodoncia">
+          <rect x="756" y="296" fill="#fff" opacity="0" width="150" height="189"></rect>
         </a>
       </svg>
     </div>
-  </header>
 
-  <main class="intro-secc-esp">
-    <h2 class="f1"><b>Salud,</b> motivo para sonreír</h2>
-    <div class="f1 txt-esp">
-      <p>¿Qué implica sonreír? Además de un estado de ánimo, una boca libre de molestias y una dentadura que brinde
-        confianza son fundamentales para realizar ese acto que puede ser expresión de plenitud y de empatía.</p>
-      <p>Mucho se ha dicho que sonreír puede darle un giro a cualquier situación, pero es verdad que, desde el ámbito de
-        la salud, habría que comenzar por hablar sobre lo mucho que puede cambiar la vida de un individuo al recuperar
-        el bienestar de su boca. Por ello, dedicamos esta edición a la salud bucal integral.</p>
+    <div class="intro-esp">
+      <h2 class="f1"><b>Salud,</b> motivo para sonreír</h2>
+      <div class="f1 txt-esp">
+        <p>¿Qué implica sonreír? Además de un estado de ánimo, una boca libre de molestias y una dentadura que brinde
+          confianza son fundamentales para realizar ese acto que puede ser expresión de plenitud y de empatía.</p>
+        <p>Mucho se ha dicho que sonreír puede darle un giro a cualquier situación, pero es verdad que, desde el ámbito de
+          la salud, habría que comenzar por hablar sobre lo mucho que puede cambiar la vida de un individuo al recuperar
+          el bienestar de su boca. Por ello, dedicamos esta edición a la salud bucal integral.</p>
+      </div>
     </div>
   </main>
 
-  <?php include ('../../../includes/footer.php') ?>
+  <?php include('../../../includes/footer.php') ?>
 </body>
+
 </html>
