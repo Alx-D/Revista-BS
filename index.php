d1b8ac020368ce60e1e8e9c8f56edf3e5b869844
Alx-D <duartepublicidad@gmail.com>
2023-01-10 22:01:32 -0700

    10ene23 10pm

--- index.php
+++ index.php
@@ -54,145 +54,94 @@
   <div class="clear"></div>
 
   <!-- 1EDICIONES -->
-  <section class="ediciones">
+  <section class="contenedor m-0a">
 
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/270/Revista-BS-270.pdf" target="blank">
-        <img src="ediciones/2022/270/BS270-portada.jpg" title="Revista BS #270" />
-        <h4 class="f1 hv-txt-01">Revista BS #270</h4>
-      </a>
+    <div class="ediciones-cont">
+      <div class="edicion-bx">
+        <a class="pdf-desk" href="ediciones/2022/270/Revista-BS-270.pdf" target="blank">
+          <img src="ediciones/2022/270/BS270-portada.jpg" title="Revista BS #270" />
+          <h4 class="f1 hv-txt-01">Revista BS #270</h4>
+        </a>
 
-      <a class="pdf-mob" href="ediciones/2022/270/Revista-BS-270-m.pdf" target="blank">
-        <img src="ediciones/2022/270/BS270-portada.jpg" title="Revista BS #270" />
-        <h4 class="f1 hv-txt-01">Revista BS #270</h4>
-      </a>
-    </div>
+        <a class="pdf-mob" href="ediciones/2022/270/Revista-BS-270-m.pdf" target="blank">
+          <img src="ediciones/2022/270/BS270-portada.jpg" title="Revista BS #270" />
+          <h4 class="f1 hv-txt-01">Revista BS #270</h4>
+        </a>
+      </div>
 
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/269/Revista-BS-269.pdf" target="blank">
-        <img src="ediciones/2022/269/BS269-portada.jpg" title="Revista BS #269" />
-        <h4 class="f1 hv-txt-01">Revista BS #269</h4>
-      </a>
+      <div class="edicion-bx">
+        <a class="pdf-desk" href="ediciones/2022/269/Revista-BS-269.pdf" target="blank">
+          <img src="ediciones/2022/269/BS269-portada.jpg" title="Revista BS #269" />
+          <h4 class="f1 hv-txt-01">Revista BS #269</h4>
+        </a>
 
-      <a class="pdf-mob" href="ediciones/2022/269/Revista-BS-269-m.pdf" target="blank">
-        <img src="ediciones/2022/269/BS269-portada.jpg" title="Revista BS #269" />
-        <h4 class="f1 hv-txt-01">Revista BS #269</h4>
-      </a>
-    </div>
+        <a class="pdf-mob" href="ediciones/2022/269/Revista-BS-269-m.pdf" target="blank">
+          <img src="ediciones/2022/269/BS269-portada.jpg" title="Revista BS #269" />
+          <h4 class="f1 hv-txt-01">Revista BS #269</h4>
+        </a>
+      </div>
 
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/268/Revista-BS-268.pdf" target="blank">
-        <img src="ediciones/2022/268/BS268-portada.jpg" title="Revista BS #268" />
-        <h4 class="f1 hv-txt-01">Revista BS #268</h4>
-      </a>
+      <div class="edicion-bx">
+        <a class="pdf-desk" href="ediciones/2022/268/Revista-BS-268.pdf" target="blank">
+          <img src="ediciones/2022/268/BS268-portada.jpg" title="Revista BS #268" />
+          <h4 class="f1 hv-txt-01">Revista BS #268</h4>
+        </a>
 
-      <a class="pdf-mob" href="ediciones/2022/268/Revista-BS-268-m.pdf" target="blank">
-        <img src="ediciones/2022/268/BS268-portada.jpg" title="Revista BS #268" />
-        <h4 class="f1 hv-txt-01">Revista BS #268</h4>
-      </a>
-    </div>
+        <a class="pdf-mob" href="ediciones/2022/268/Revista-BS-268-m.pdf" target="blank">
+          <img src="ediciones/2022/268/BS268-portada.jpg" title="Revista BS #268" />
+          <h4 class="f1 hv-txt-01">Revista BS #268</h4>
+        </a>
+      </div>
 
-    <div class="ediciones-bx">
+      <div class="edicion-bx">
+        <div class="pdf-desk">
+          <img src="ediciones/2022/267/BS267-portadas.gif" title="Revista BS #267">
+          <div>
+            <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp1.pdf" target="_blank">
+              <h4>#267 Especial 1</h4>
+            </a>
+            <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp2.pdf" target="_blank">
+              <h4>#267 Especial 2</h4>
+            </a>
+          </div>
+        </div>
 
-      <div class="pdf-desk ediciones-dbl-bx">
-        <img src="ediciones/2022/267/BS267-portadas.gif" title="Revista BS #267">
-        <div>
-          <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp1.pdf" target="_blank">
-            <h4>#267 Especial 1</h4>
-          </a>
-          <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp2.pdf" target="_blank">
-            <h4>#267 Especial 2</h4>
-          </a>
+        <div class="pdf-mob">
+          <img src="ediciones/2022/267/BS267-portadas.gif" title="Revista BS #267">
+          <div>
+            <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp1-m.pdf" target="_blank">
+              <h4>#267 Especial 1</h4>
+            </a>
+            <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp2-m.pdf" target="_blank">
+              <h4>#267 Especial 2</h4>
+            </a>
+          </div>
         </div>
       </div>
 
-      <div class="pdf-mob ediciones-dbl-bx">
-        <img src="ediciones/2022/267/BS267-portadas.gif" title="Revista BS #267">
-        <div>
-          <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp1-m.pdf" target="_blank">
-            <h4>#267 Especial 1</h4>
-          </a>
-          <a class="f1 hv-txt-01" href="ediciones/2022/267/Revista-BS-267-Esp2-m.pdf" target="_blank">
-            <h4>#267 Especial 2</h4>
-          </a>
-        </div>
+      <div class="edicion-bx">
+        <a class="pdf-desk" href="ediciones/2022/266/Revista-BS-266.pdf" target="blank">
+          <img src="ediciones/2022/266/BS266-portada.jpg" title="Revista BS #266" />
+          <h4 class="f1 hv-txt-01">Revista BS #266</h4>
+        </a>
+
+        <a class="pdf-mob" href="ediciones/2022/266/Revista-BS-266-m.pdf" target="blank">
+          <img src="ediciones/2022/266/BS266-portada.jpg" title="Revista BS #266" />
+          <h4 class="f1 hv-txt-01">Revista BS #266</h4>
+        </a>
       </div>
 
-      <!-- <div>
-        <a class="pdf-desk" href="ediciones/2022/267/Revista-BS-267-Esp1.pdf" target="blank">
-          <img src="ediciones/2022/267/BS267-portada-Especial1.jpg" title="Revista BS #267 especial 1" />
-          <h4 class="f1 hv-txt-01">Revista BS #267 especial 1</h4>
+      <div class="edicion-bx">
+        <a class="pdf-desk" href="ediciones/2022/265/Revista-BS-265.pdf" target="blank">
+          <img src="ediciones/2022/265/BS265-portada.jpg" title="Revista BS #265" />
+          <h4 class="f1 hv-txt-01">Revista BS #265</h4>
         </a>
 
-        <a class="pdf-mob" href="ediciones/2022/267/Revista-BS-267-Esp1-m.pdf" target="blank">
-          <img src="ediciones/2022/267/BS267-portada-Especial1.jpg" title="Revista BS #267 especial 1" />
-          <h4 class="f1 hv-txt-01">Revista BS #267 especial 1</h4>
+        <a class="pdf-mob" href="ediciones/2022/265/Revista-BS-265-m.pdf" target="blank">
+          <img src="ediciones/2022/265/BS265-portada.jpg" title="Revista BS #265" />
+          <h4 class="f1 hv-txt-01">Revista BS #265</h4>
         </a>
-      </div> -->
-
-      <!-- <div>
-        <a class="pdf-desk" href="ediciones/2022/267/Revista-BS-267-Esp2.pdf" target="blank">
-          <img src="ediciones/2022/267/BS267-portada-Especial2.jpg" title="Revista BS #267 especial 2" />
-          <h4 class="f1 hv-txt-01">Revista BS #267 especial 2</h4>
-        </a>
-
-        <a class="pdf-mob" href="ediciones/2022/267/Revista-BS-267-Esp2-m.pdf" target="blank">
-          <img src="ediciones/2022/267/BS267-portada-Especial2.jpg" title="Revista BS #267 especial 2" />
-          <h4 class="f1 hv-txt-01">Revista BS #267 especial 2</h4>
-        </a>
-      </div> -->
-
-    </div>
-
-    <!-- BANNER PORTADA 2 -->
-    <?php include('includes/bnr-hz-02.php') ?>
-
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/266/Revista-BS-266.pdf" target="blank">
-        <img src="ediciones/2022/266/BS266-portada.jpg" title="Revista BS #266" />
-        <h4 class="f1 hv-txt-01">Revista BS #266</h4>
-      </a>
-
-      <a class="pdf-mob" href="ediciones/2022/266/Revista-BS-266-m.pdf" target="blank">
-        <img src="ediciones/2022/266/BS266-portada.jpg" title="Revista BS #266" />
-        <h4 class="f1 hv-txt-01">Revista BS #266</h4>
-      </a>
-    </div>
-
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/265/Revista-BS-265.pdf" target="blank">
-        <img src="ediciones/2022/265/BS265-portada.jpg" title="Revista BS #265" />
-        <h4 class="f1 hv-txt-01">Revista BS #265</h4>
-      </a>
-
-      <a class="pdf-mob" href="ediciones/2022/265/Revista-BS-265-m.pdf" target="blank">
-        <img src="ediciones/2022/265/BS265-portada.jpg" title="Revista BS #265" />
-        <h4 class="f1 hv-txt-01">Revista BS #265</h4>
-      </a>
-    </div>
-
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/264/Revista-BS-264.pdf" target="blank">
-        <img src="ediciones/2022/264/BS264-portada.jpg" title="Revista BS #264" />
-        <h4 class="f1 hv-txt-01">Revista BS #264</h4>
-      </a>
-
-      <a class="pdf-mob" href="ediciones/2022/264/Revista-BS-264-m.pdf" target="blank">
-        <img src="ediciones/2022/264/BS264-portada.jpg" title="Revista BS #264" />
-        <h4 class="f1 hv-txt-01">Revista BS #264</h4>
-      </a>
-    </div>
-
-    <div class="ediciones-bx">
-      <a class="pdf-desk" href="ediciones/2022/263/Revista-BS-263.pdf" target="blank">
-        <img src="ediciones/2022/263/BS263-portada.jpg" title="Revista BS #263" />
-        <h4 class="f1 hv-txt-01">Revista BS #263</h4>
-      </a>
-
-      <a class="pdf-mob" href="ediciones/2022/263/Revista-BS-263-m.pdf" target="blank">
-        <img src="ediciones/2022/263/BS263-portada.jpg" title="Revista BS #263" />
-        <h4 class="f1 hv-txt-01">Revista BS #263</h4>
-      </a>
+      </div>
     </div>
   </section>
 
@@ -200,25 +149,25 @@
   <?php include('includes/bnr-hz-01.php') ?>
 
   <!-- 1VIDEOS -->
-  <section class="videos">
+  <section class="videos-bk">
 
     <div class="contenedor m-0a">
-      <div class="frames">
+      <div class="videos-cont">
 
-        <div class="videos-bx">
+        <div class="video-bx">
           <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F525258101972060%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true">
           </iframe>
         </div>
 
-        <div class="videos-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F871091170163519%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
+        <div class="video-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F871091170163519%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
 
-        <div class="videos-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F1381143148904762%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
+        <div class="video-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F1381143148904762%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
 
-        <div class="videos-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F742883103021631%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
+        <div class="video-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F742883103021631%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
 
-        <div class="videos-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F4120319731331400%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
+        <div class="video-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F4120319731331400%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
 
-        <div class="videos-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F2025613831073432%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
+        <div class="video-bx"><iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FRevistaBS%2Fvideos%2F2025613831073432%2F&show_text=true&width=560&t=0" width="100%" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe></div>
       </div>
     </div>
   </section>
